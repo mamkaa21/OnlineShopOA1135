c316f4964ab52ca639230e5891d804eb093befe9
mamka <mamka21a@mail.ru>
2025-04-07 19:17:24 +1000

    работать и работатт.......

--- OnlineShopOA1135/ViewModel/AdminWinVM.cs
+++ OnlineShopOA1135/ViewModel/AdminWinVM.cs
@@ -11,14 +11,44 @@ namespace OnlineShopOA1135.ViewModel
     public class AdminWinVM : BaseVM
     {
         public Command OrderWinOpen { get; }
+        public Command CreateWinOpen { get; }
+        public Command EditWinOpen { get; }
+        public Command DeleteGoods {  get; }
+        public Command FindGoodsByTitle { get; }
+        public Command UserWinOpen { get; }
         public AdminWinVM() 
         {
             OrderWinOpen = new Command(() =>
             {
-
+                OrderWin orderWin = new OrderWin();
+                orderWin.Show();
+                Signal();
             });
-        
-        
+            CreateWinOpen = new Command(() => 
+            { 
+                CreateEditWin createEditWin = new CreateEditWin();
+                createEditWin.Show();
+                Signal();
+            });
+            EditWinOpen = new Command(() => 
+            { 
+                //проверка что обьект не пустой и только тогда открыть окно
+            });
+            DeleteGoods = new Command(() =>
+            {
+                //запрос на удаление товара + проверка, что товар не пустой
+            });
+            FindGoodsByTitle = new Command(() => 
+            {
+                //запрос на поиск товара 
+            });
+            UserWinOpen = new Command(() => 
+            { 
+                UserWin userWin = new UserWin();
+                userWin.Show();
+                Signal();
+            });
         }
+
     }
 }
