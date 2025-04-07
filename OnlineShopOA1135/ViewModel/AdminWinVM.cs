58621e219efc6629d6d5b95059aadff430e90cf7
mamka <mamka21a@mail.ru>
2025-04-07 18:30:16 +1000

    сделала окна и команды чутка надо дальше делать умоляю алина надо работать

--- OnlineShopOA1135/ViewModel/AdminWinVM.cs
+++ OnlineShopOA1135/ViewModel/AdminWinVM.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OnlineShopOA1135.Model;
+using OnlineShopOA1135.View;
 
 namespace OnlineShopOA1135.ViewModel
 {
-    public class AdminWinVM
+    public class AdminWinVM : BaseVM
     {
+        public Command OrderWinOpen { get; }
+        public AdminWinVM() 
+        {
+            OrderWinOpen = new Command(() =>
+            {
+
+            });
+        
+        
+        }
     }
 }
