0a61829cc7bf51908feae9b6eb57d0b351c62839
mamka <mamka21a@mail.ru>
2025-04-14 20:43:09 +1000

    почему то не выводится

--- OnlineShopOA1135/ViewModel/RegisterWnVM.cs
+++ OnlineShopOA1135/ViewModel/RegisterWnVM.cs
@@ -5,18 +5,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OnlineShopOA1135.Model;
+using System.Net.Http;
+using System.Text.Json;
+using System.Windows;
+using System.Net.Http.Json;
 
 namespace OnlineShopOA1135.ViewModel
 {
-    public class RegisterWnVM
+    public class RegisterWnVM : BaseVM
     {
-        public User User = new User();
+        public User User { get; set;  } = new ();
         public Command EnterWinOpen { get; }
         public RegisterWnVM() 
         {
             EnterWinOpen = new Command(async() =>
             {
+                string arg = JsonSerializer.Serialize(User);
+                var responce = await HttpClientS.HttpClient.PostAsync($"Auth/AddNewUser", new StringContent(arg, Encoding.UTF8, "application/json"));
 
+                if (responce.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    var result = await responce.Content.ReadAsStringAsync();
+                    MessageBox.Show("Пароль или логин не может быть пустым");
+                    return;
+                }
+                if (responce.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    EnterWin enterWin = new EnterWin();
+                    enterWin.Show();
+                    Signal();
+                }
             });
         }
         RegisterWin registerWindow;
--- OnlineShopOA1135/ViewModel/UserMenuVM.cs
+++ OnlineShopOA1135/ViewModel/UserMenuVM.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
 using OnlineShopOA1135.Model;
 using OnlineShopOA1135.View;
 
@@ -11,10 +14,33 @@ namespace OnlineShopOA1135.ViewModel
 {
     public class UserMenuVM : BaseVM
     {
+        private Good good { get; set; }
+        public Good Good 
+        { 
+            get => good;
+            set
+            {
+                good = value;
+                Signal(nameof(Good));
+            }
+        }
+
+        private List<Good> goodList { get; set; }
+        public List<Good> GoodList
+        {
+            get => goodList;
+            set
+            {
+                goodList = value;
+                Signal(nameof(GoodList));
+            }
+        }
+
         public Command BasketWinOpen { get; }
         public Command UserWinOpen { get; }
         public UserMenuVM()
         {
+            GetGoods();
             BasketWinOpen = new Command(() =>
             {
                 BasketWin basketWin = new BasketWin();
@@ -28,5 +54,24 @@ namespace OnlineShopOA1135.ViewModel
                 Signal();
             });
         }
+
+        public async void GetGoods()
+        {
+            string arg = JsonSerializer.Serialize(Good);
+            var responce = await HttpClientS.HttpClient.GetAsync($"Admin/getGoods");
+
+            if (responce.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var result = await responce.Content.ReadAsStringAsync();
+                MessageBox.Show(result);
+                return;
+            }
+            if (responce.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                MessageBox.Show("succes");
+                return;
+            }
+        }
+
     }
 }
