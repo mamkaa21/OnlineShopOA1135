eb66f68022bf3fcbb6135016c4bc6fd295f732d0
mamka <mamka21a@mail.ru>
2025-04-15 15:39:43 +1000

    ультую сегодня

--- OnlineShopOA1135/ViewModel/AdminWinVM.cs
+++ OnlineShopOA1135/ViewModel/AdminWinVM.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
 using OnlineShopOA1135.Model;
 using OnlineShopOA1135.View;
 
@@ -10,6 +13,28 @@ namespace OnlineShopOA1135.ViewModel
 {
     public class AdminWinVM : BaseVM
     {
+        private Good good { get; set; }
+        public Good Good
+        {
+            get => good;
+            set
+            {
+                good = value;
+                Signal(nameof(Good));
+            }
+        }
+
+        private List<Good> goodList { get; set; }
+        public List<Good> GoodList
+        {
+            get => goodList;
+            set
+            {
+                goodList = value;
+                Signal(nameof(GoodList));
+            }
+        }
+
         public Command OrderWinOpen { get; }
         public Command CreateWinOpen { get; }
         public Command EditWinOpen { get; }
@@ -18,6 +43,7 @@ namespace OnlineShopOA1135.ViewModel
         public Command UserWinOpen { get; }
         public AdminWinVM() 
         {
+            GetGoods();
             OrderWinOpen = new Command(() =>
             {
                 OrderWin orderWin = new OrderWin();
@@ -49,6 +75,23 @@ namespace OnlineShopOA1135.ViewModel
                 Signal();
             });
         }
+        public async void GetGoods()
+        {
+            string arg = JsonSerializer.Serialize(Good);
+            var responce = await HttpClientS.HttpClient.GetAsync($"Admin/getGoods");
 
+            if (responce.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var result = await responce.Content.ReadAsStringAsync();
+                MessageBox.Show(result);
+                return;
+            }
+            if (responce.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                MessageBox.Show("succes");
+                GoodList = await responce.Content.ReadFromJsonAsync<List<Good>>();
+                return;
+            }
+        }
     }
 }
--- OnlineShopOA1135/ViewModel/CreateEditWinVM.cs
+++ OnlineShopOA1135/ViewModel/CreateEditWinVM.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using OnlineShopOA1135.Model;
+using OnlineShopOA1135.View;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace OnlineShopOA1135.ViewModel
 {
-    class CreateEditWinVM
+    internal class CreateEditWinVM : BaseVM
     {
+        public Good Good { get; set; } = new Good();
+
+        public Command GetImageByFile { get; }
+        public Command AddGoods { get; }
+
+        
+        public CreateEditWinVM()
+        {
+           
+
+            GetImageByFile = new Command(() =>
+            {
+               OpenFileDialog openFileDialog = new OpenFileDialog();
+               if( openFileDialog.ShowDialog() == true)
+               {
+                    var filepath = openFileDialog.FileName;
+                    var info = new FileInfo(filepath);
+                    if (!info.Exists || info.Length > 2 * 1024 * 1024)
+                    {
+                        MessageBox.Show("Файл имеет неверный размер");
+                        return;
+                    }
+                    Good.Image =  File.ReadAllBytes(filepath);
+                    Signal(nameof(Good));
+               }
+            });
+            AddGoods = new Command(async () =>
+            {
+                string arg = JsonSerializer.Serialize(Good);
+                var responce = await HttpClientS.HttpClient.PostAsync($"Admin/CreateGoods", new StringContent(arg, Encoding.UTF8, "application/json"));
+
+                if (responce.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    var result = await responce.Content.ReadAsStringAsync();
+                    MessageBox.Show("error");
+                    return;
+                }
+                if (responce.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    MessageBox.Show("ok");
+                }
+
+            });
+        }
     }
 }
