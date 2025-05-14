63cd5365fcb6890794df0243845fea613b4bb063
mamka <mamka21a@mail.ru>
2025-05-14 13:23:28 +1000

    hhh

--- OnlineShopOA1135/ViewModel/GoodWinVM.cs
+++ OnlineShopOA1135/ViewModel/GoodWinVM.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using OnlineShopOA1135.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using OnlineShopOA1135.View;
 
 namespace OnlineShopOA1135.ViewModel
 {
-    class GoodWinVM
+    public class GoodWinVM : BaseVM
     {
+        private Good good { get; set; }
+        public Good Good
+        {
+            get => good;
+            set
+            {
+                good = value;
+                Signal(nameof(Good));
+            }
+        }
+
+        private Category category { get; set; }
+        public Category Category
+        {
+            get => category;
+            set
+            {
+                category = value;
+                Signal(nameof(Category));
+            }
+        }
+
+        public Command CloseGoodWin { get; }
+
+      public GoodWinVM() 
+      {
+            CloseGoodWin = new Command(() => 
+            {
+                GoodWin goodWin = new GoodWin();
+                goodWin.Close();
+            
+            });
+      }
+
     }
 }
